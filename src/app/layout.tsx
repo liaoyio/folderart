95f73bd6c899ae1a0d18e95db363e81c78ce4130
liaoyi <2417276459@qq.com>
2025-06-20 22:09:25 +0800

    feat(seo): 新增  SEO :sparkles:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,19 @@
+import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+import { NextIntlClientProvider, useLocale } from 'next-intl'
+import { getLocale, getTranslations } from 'next-intl/server'
+import Header from '@/components/header'
+
 import { ThemeProvider } from '@/components/next-theme/provider'
 import { geistMono, geistSans } from '@/fonts'
 import { cn } from '@/lib/cn'
 import { baseUrl } from '@/lib/utils'
 import '../css/style.css'
 
-export const metadata = {
-  title: 'FolderArt',
-  metadataBase: baseUrl,
-  icon: 'icon.svg',
-}
-
-import { NextIntlClientProvider } from 'next-intl'
-import { getLocale } from 'next-intl/server'
-import Header from '@/components/header'
-
 export default async function Layout({ children }: { children: ReactNode }) {
   const locale = await getLocale()
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+    <html lang={locale} className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
       <body className={cn('relative flex h-svh flex-col overflo-hidden')}>
         <ThemeProvider attribute="class">
           <NextIntlClientProvider locale={locale}>
@@ -29,4 +24,41 @@ export default async function Layout({ children }: { children: ReactNode }) {
       </body>
     </html>
   )
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations('metaData')
+  return {
+    title: t('title'),
+    metadataBase: baseUrl,
+    description: t('description'),
+    keywords: t('keywords'),
+    icons: { icon: '/icon.png' },
+    openGraph: {
+      title: t('title'),
+      url: '/',
+      description: t('description'),
+      images: [
+        {
+          url: '/cover.png',
+          width: 1200,
+          height: 630,
+          alt: t('title'),
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('title'),
+      description: t('description'),
+      images: ['/cover.png'],
+    },
+    alternates: {
+      canonical: '/',
+      languages: {
+        en: '/en',
+        zh: '/zh',
+      },
+    },
+  }
 }
